7807f5dca7bdacac884277d83d82a916e04d8e4e
Sanjib Roy <sanjibroy0098@gmail.com>
2020-09-06 16:46:42 +0530

    fixed routes-index bug

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
-var hash = "bundle.c06bc51596b6b3191a89";
+
+var hash = 'bundle.c06bc51596b6b3191a89';
 
 /* GET home page. */
 router.get("*", function (req, res, next) {
