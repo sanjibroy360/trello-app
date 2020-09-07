e5d1afdfb85db7d772f61373bcb2d3e65d6f7045
Sanjib Roy <sanjibroy0098@gmail.com>
2020-09-07 19:02:35 +0530

    new bundle client-index.js css change

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./views/App";
-import "./scss/index.scss";
+import "./scss/index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
