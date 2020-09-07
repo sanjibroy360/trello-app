6ddb7377cf5f462f5feafafc56477bbeed38a442
Sanjib Roy <sanjibroy0098@gmail.com>
2020-09-07 17:31:01 +0530

    secret manually

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,7 +6,7 @@ exports.generateToken = function (user) {
     email: user.email,
     password: user.password,
   };
-  const token = sign(payload, process.env.SECRET);
+  const token = sign(payload, "sanjib");
   return token;
 };
 
@@ -22,22 +22,21 @@ exports.verifyToken = async function (req, res, next) {
         authorization[1]) ||
       null;
 
-    if (!token || token=="undefined") {
+    if (!token || token == "undefined") {
       req.user = null;
       console.log("returned");
       next();
       return;
       // return res.status(401).json({ message: `Unauthorized user!` });
     }
-    
 
-    let { userId } = await verify(token, process.env.SECRET);
+    let { userId } = await verify(token, "sanjib");
     if (!userId) {
       next();
       return res.status(401).json({ message: `Unauthorized user!` });
     }
     req.user = { userId, token };
-    console.log({Req: req.user})
+    console.log({ Req: req.user });
     next();
   } catch (error) {
     next(error);
